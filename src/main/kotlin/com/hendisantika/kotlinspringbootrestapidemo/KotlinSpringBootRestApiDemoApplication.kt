52ad402e7f46d3f022df2a8f366a84a6e1d2e6bd
Hendi Santika <hendisantika@gmail.com>
2018-01-17 06:09:13 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/kotlinspringbootrestapidemo/KotlinSpringBootRestApiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.kotlinspringbootrestapidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootRestApiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootRestApiDemoApplication>(*args)
+}
